6421405ec1e5b260303fc96cd701ce7ed15f0719
benjamenriquez <benjamenriquez@gmail.com>
2017-06-18 19:54:59 -0300

    creacion proyecto drools

---
+++ inversiones/src/main/java/com/grupo4/inversiones/entidades/Metodologia.java
@@ -0,0 +1,11 @@
+package com.grupo4.inversiones.entidades;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Metodologia {
+	
+	private String nombre;
+	private List<Condicion> condiciones = new ArrayList<Condicion>();
+
+}
