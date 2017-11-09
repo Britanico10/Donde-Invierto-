4efedb7bacac193ff6ae078b77340c5d3d1da839
benjamenriquez <benjamenriquez@gmail.com>
2017-11-08 21:42:10 -0300

    inicio de sesion del cliente andando

--- ddspagina/bootstrap/grid/js/login.js
+++ ddspagina/bootstrap/grid/js/login.js
@@ -5,22 +5,23 @@ function login(){
 		alert("Faltan datos");
 	}
 	else {
-        //var dataString = $("#datosFormulario").serialize();
-		$.ajax({
-                url: "http://localhost:8080/api/iniciarsesion",
-                method: "GET",
-                async: true,
-                contentType: "text/json; charset=utf-8",
-                processData: false,
-                //dataType: "jsonp",
-                //data: dataString       
-                data:"nombre="+user+"&password="+pass,
-            }).fail(function( jqXHR, textStatus, errorThrown ) {
-                console.log("aca");
-                alert(jqXHR.status);
-            }).success(function() {
-                console.log("Ponzio");
-            });
+        $.ajax({
+            method: "GET",
+            url: "http://localhost:8080/api/iniciarsesion?nombre="+user+"&password="+pass,
+            contentType: "application/json;charset=UTF-8",
+            dataType: "json",
+            async: false
+             })
+          .done(function(data) {
+            alert( "success" );
+            alert(data.token);
+          })
+          .fail(function() {
+            alert( "error" );
+          })
+          .always(function() {
+            alert( "complete" );
+          });
 	}
 }
 
