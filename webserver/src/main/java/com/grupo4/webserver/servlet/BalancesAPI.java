b87fb0a204d785a8238b2faa4c5fc6bc072fa29b
benjamenriquez <benjamenriquez@gmail.com>
2017-12-04 23:22:26 -0300

    import

--- webserver/src/main/java/com/grupo4/webserver/servlet/BalancesAPI.java
+++ webserver/src/main/java/com/grupo4/webserver/servlet/BalancesAPI.java
@@ -8,6 +8,8 @@ import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
